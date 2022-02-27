f67d613b11da7f1abc71241e60c4bb763d198212
Jaan Sangha <jaansangha10@gmail.com>
2022-02-26 20:28:27 -0500

    added barrier walls

--- Assets/Scripts/WaterMovement.cs
+++ Assets/Scripts/WaterMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class WaterMovement : MonoBehaviour
 {
+    private float yPos;
+    public float waterSpeed = 0.3f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        yPos = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       // yPos += Time.deltaTime;
+        //transform.position = new Vector3(transform.position.x, yPos * waterSpeed, transform.position.z);
     }
 }
